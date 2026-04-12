bcaaf408fb678e8b97928cbf04acd630cc81b66f
blumm748 <blumm748@newschool.edu>
2026-04-12 17:33:27 -0400

    Update particle.js

--- particle.js
+++ particle.js
@@ -25,10 +25,10 @@ class Particle {
   constructor(x, y) {
     this.position = createVector(x, y);
     this.velocity = p5.Vector.random2D();
-    this.velocity.mult(random(1, 6));
+    this.velocity.mult(random(1, 3));
     this.acceleration = createVector(0, 0);
     this.mass = random(2, 6);
-    this.r = sqrt(this.mass) * 20;
+    this.r = sqrt(this.mass) * 10;
     // images!
     this.img = random(particleImages);
     this.angle = random(TWO_PI);
